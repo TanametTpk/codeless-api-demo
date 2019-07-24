818c340ea9be948cb2ca85088f421daaf4c25439
tanamet.tpk <tanamet.tpk@gmail.com>
2019-07-24 09:08:18 +0700

    + init files

---
+++ precache-manifest.d87ddcb0a8acb78636b24e2f4eaedab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc34464f32583f6b35270449ace5a639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eea3ef8a8a5685f85b2",
+    "url": "/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "9aa91d773d260861527f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7eea3ef8a8a5685f85b2",
+    "url": "/static/js/2.8659ea53.chunk.js"
+  },
+  {
+    "revision": "9aa91d773d260861527f",
+    "url": "/static/js/main.8de032ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
